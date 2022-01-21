52f3c2a3243b05d3f2c2593864d7470e16344817
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2022-01-21 12:52:52 -0500

    Added character controller and script that updates every frame

---
+++ Assets/RotatorScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorScript : MonoBehaviour
+{
+
+    public float rotationSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      this.GetComponent<Transform>().Rotate(0,rotationSpeed,0);
+      /*if(Input.GetKey(KeyCode.R)) {
+        this.GetComponent<Transform>().Rotate(0,rotationSpeed,0);
+      }
+      if(Input.GetKey(KeyCode.T)) {
+        this.GetComponent<Transform>().Rotate(0,-rotationSpeed,0);
+      }*/
+    }
+
+    /*public void Rotate1() {
+      this.GetComponent<Transform>().Rotate(0,rotationSpeed,0);
+    }
+
+    public void Rotate2() {
+      this.GetComponent<Transform>().Rotate(0,-rotationSpeed,0);
+    }*/
+}
